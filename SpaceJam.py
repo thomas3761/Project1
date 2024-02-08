d874f92c12dc14d59748cc076370c9357cb3463f
thomas3761 <102927056+thomas3761@users.noreply.github.com>
2024-02-08 15:57:41 -0500

    change test

--- SpaceJam.py
+++ SpaceJam.py
@@ -1,22 +1,60 @@
 from direct.showbase.ShowBase import ShowBase
+
 class SpaceJam(ShowBase):
     def __init__(self):
         ShowBase.__init__(self)
-        self.SetScene()
-    
-    def SetScene(self):
-        self.Universe = self.loader.loadModel("./Assets/Universe/Universe.x")
-        self.Universe.rearentTo(self.render)
-        self.Universe.setScale(15000)
-        self.Planet1 = self.loader.loadModel("./Assets/Planets/protoPlanet.x")
-        self.Planet1.rearentTo(self.render)
-        self.Planet1.setPos(150,5000,67)
-        self.Planet1.setScale(350)
+        self.setScene()
+
+    def setScene(self):
+        
+        self.universe = self.loader.loadModel("./Assets/Universe/Universe.x")
+        self.universe.reparentTo(self.render)
+        self.universe.setScale(15000)
         tex = self.loader.loadTexture("./Assets/Universe/space-galaxy.jpg")
-        self.Universe.setTexture(tex,1)       
+        self.universe.setTexture(tex, 1)
 
-    
+        #planets
+        self.planet1 = self.loader.loadModel("./Assets/Planets/protoPlanet.x")
+        self.planet1.reparentTo(self.render)
+        self.planet1.setPos(150, 5000, 67)
+        self.planet1.setScale(350)
+        tex = self.loader.loadTexture("./Assets/Planets/planet 1.jpg")
+        self.planet1.setTexture(tex, 1)
 
+        self.planet2 = self.loader.loadModel("./Assets/Planets/protoPlanet.x")
+        self.planet2.reparentTo(self.render)
+        self.planet2.setPos(7314, 1274, 976)
+        self.planet2.setScale(350)
+        tex = self.loader.loadTexture("./Assets/Planets/planet 2.jpg")
+        self.planet2.setTexture(tex, 1)
+
+        self.planet3 = self.loader.loadModel("./Assets/Planets/protoPlanet.x")
+        self.planet3.reparentTo(self.render)
+        self.planet3.setPos(11985, 5489, 1112)
+        self.planet3.setScale(350)
+        tex = self.loader.loadTexture("./Assets/Planets/planet 3.png")
+        self.planet3.setTexture(tex, 1)
+
+        self.planet4 = self.loader.loadModel("./Assets/Planets/protoPlanet.x")
+        self.planet4.reparentTo(self.render)
+        self.planet4.setPos(9067, 13832, 2378)
+        self.planet4.setScale(350)
+        tex = self.loader.loadTexture("./Assets/Planets/planet 4.jpg")
+        self.planet4.setTexture(tex, 1)
+
+        self.planet5 = self.loader.loadModel("./Assets/Planets/protoPlanet.x")
+        self.planet5.reparentTo(self.render)
+        self.planet5.setPos(10382, 728, 14567)
+        self.planet5.setScale(350)
+        tex = self.loader.loadTexture("./Assets/Planets/planet 5.jpg")
+        self.planet5.setTexture(tex, 1)
+
+        self.planet6 = self.loader.loadModel("./Assets/Planets/protoPlanet.x")
+        self.planet6.reparentTo(self.render)
+        self.planet6.setPos(14502, 9321, 6478)
+        self.planet6.setScale(350)
+        tex = self.loader.loadTexture("./Assets/Planets/planet 6.png")
+        self.planet6.setTexture(tex, 1)
 
 
 app = SpaceJam()
